9b2694bdef31ee9cc84c6abdc47d5fd0cb1bce35
Julie Chaparro <adachaparro3@gmail.com>
2021-10-13 22:34:07 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distanceForGreeting) {
+  if (distanceForGreeting <= 400) {
+    return "This one is on me!"
+  }
+  else if (distanceForGreeting >= 2000 && distanceForGreeting <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (distanceForGreeting > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(someCity){
+  if (someCity == "NYC") {
+    return "Ok, sounds good."
+  }
+  else if (someCity != "NYC") {
+    return "No go."
+  }
+ return ternaryCheckCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount) {
+  case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  case 'thanks for everything':
+    return 'Bye.'
+}
+  return switchOnCharmFromTip;
 }
